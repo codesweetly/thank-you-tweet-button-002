2941be468d3aedecd53240ecf54b7aa76ef9ad51
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-01-09 14:24:45 +0100

    style: create the tweet button's stylesheet

--- TweetButton.tsx
+++ TweetButton.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./index.css";
 
 export function TweetButton() {
   const tweetURL =
     "https://twitter.com/intent/tweet?text=Thank+you,+%40oluwatobiss.+Your+book+helped+me+create,+test,+and+publish+an+NPM+package.%0A%0ACreating%20NPM%20Package:%20React%20TypeScript%20Guide%0A%0Ahttps%3A%2F%2Famzn.to/3R1M0XU";
 
   return (
-    <div>
-      <a href={tweetURL} target="_blank">
+    <div className="tweet-btn-container">
+      <a className="tweet-button" href={tweetURL} target="_blank">
         Send a thank you tweet
       </a>
     </div>
--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   testEnvironment: "jsdom",
   transform: {
+    "^.+\\.css$": "jest-transform-css",
     "^.+\\.tsx?$": "ts-jest",
   },
 };
