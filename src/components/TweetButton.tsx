54ebf208282f6b9ca16bcafed6eed5f11f691e37
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-01-09 16:50:39 +0100

    refactor: change div to section element

--- src/components/TweetButton.tsx
+++ src/components/TweetButton.tsx
@@ -6,10 +6,10 @@ export function TweetButton() {
     "https://twitter.com/intent/tweet?text=Thank+you,+%40oluwatobiss.+Your+book+helped+me+create,+test,+and+publish+an+NPM+package.%0A%0ACreating%20NPM%20Package:%20React%20TypeScript%20Guide%0A%0Ahttps%3A%2F%2Famzn.to/3R1M0XU";
 
   return (
-    <div className="tweet-btn-container">
+    <section className="tweet-btn-container">
       <a className="tweet-button" href={tweetURL} target="_blank">
         Send a thank you tweet
       </a>
-    </div>
+    </section>
   );
 }
